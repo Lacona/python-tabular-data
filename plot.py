95efea946ff26702405c55d4e628815041dedc22
Lacona <azp0136@auburn.edu>
2021-03-26 10:58:28 -0500

    Error message x and y are not found in the index

--- plot.py
+++ plot.py
@@ -3,18 +3,18 @@
 import sys
 
 # define x and y and filename
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from scipy import stats
-#filename = pd.read_csv(sys.argv[2])
-#x = str(sys.argv[3])
-#y = str(sys.argv[4])   
+x = str(sys.argv[2])
+y = str(sys.argv[3])   
 
 def iris_scatter(filename, x, y):
 # import packages 
 
 # import dataset
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    from scipy import stats
+    filename = pd.read_csv(sys.argv[1])
 # create the scatterplots with regression line    
 # Explanation of the commands: hue = from which variable the groups are defined, x and y define the variables for the x and y axis, ci = this option adds a confidence interval, height changes the size of the output, x/ylabel = label x/y axis,title = adds a titel, show = the output is shown but not saved, savefig = figure is saved  
     sns.set_style("whitegrid")
@@ -30,6 +30,6 @@ def iris_scatter(filename, x, y):
 
 if __name__ == '__main__':
     import pandas as pd
-    filename = pd.read_csv(sys.argv[2])
+    filename = pd.read_csv(sys.argv[1])
     iris_scatter(filename, x, y)
 
