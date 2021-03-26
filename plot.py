0a1e7883cd84c3cf8476edbdf40b8af926ebf3f6
Lacona <azp0136@auburn.edu>
2021-03-25 21:52:37 -0500

    Added arguments and made the function applicable to different .csv files

--- plot.py
+++ plot.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
+# import pdb; pdb.set_trace()
+import sys
 
-def iris_scatter():
+def iris_scatter(filename):
 # import packages 
     import pandas as pd
     import matplotlib.pyplot as plt
@@ -8,12 +10,12 @@ def iris_scatter():
     from scipy import stats
 
 # import dataset
-    iris = pd.read_csv("iris.csv")
-
+    filename = pd.read_csv(sys.argv[1])
+   
 # create the scatterplots with regression line    
 # Explanation of the commands: hue = from which variable the groups are defined, x and y define the variables for the x and y axis, ci = this option adds a confidence interval, height changes the size of the output, x/ylabel = label x/y axis,title = adds a titel, show = the output is shown but not saved, savefig = figure is saved  
     sns.set_style("whitegrid")
-    sns.lmplot(data=iris, hue="species", x="petal_length_cm", y="sepal_length_cm", ci=None, height=6)
+    sns.lmplot(data=filename, hue="species", x="petal_length_cm", y="sepal_length_cm", ci=None, height=6)
     plt.xlabel("Pengulin")
     plt.ylabel("Dodo")
     plt.xlim(0,8)
@@ -21,31 +23,8 @@ def iris_scatter():
     
 # show and save the output
     plt.show()
-    plt.savefig("scatterplots_iris.png")
+    plt.savefig("scatterplots.png")
 
 if __name__ == '__main__':
-    iris_scatter()
-
-iris_scatter
-
-# subsetting into different dataframes:
-#def subset(dataframe, list_of_species):
-#    grouped = dataframe.query('species in @list_of_species').groupby("species")
-#    return [grouped.get_group(d) for d in list_of_species]
-#versicolor = dataframe[dataframe.species == "Iris_versicolor"]
-#virginica = (dataframe.species, ["Iris_virginica"])
-#setosa = (dataframe.species, ["Iris_setosa"])
-#print(subsets)
-
-#x = versicolor.petal_length_cm
-#y = versicolor.sepal_length_cm
-#regression = stats.linregress(x, y)
-#slope = regression.slope
-#intercept = regression.intercept
-#plt.scatter(x, y, label = '3 scatter plots')
-#plt.plot(x, slope * x + intercept, label = 'Fitted line')
-#plt.xlabel("Petal lenght in cm")
-#plt.ylabel("Sepal length in cm")
-#plt.legend()
-#plt.savefig("3-scatter.png")
+    iris_scatter(sys.argv[1])
 
