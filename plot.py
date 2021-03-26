0fc455efd4c7e850691ab909db798d9afc90b0e1
Lacona <azp0136@auburn.edu>
2021-03-25 22:12:45 -0500

    Tried to make it work for different .csv files. definition of x and y in the function sucks

--- plot.py
+++ plot.py
@@ -1,8 +1,12 @@
 #! /usr/bin/env python3
-# import pdb; pdb.set_trace()
+import pdb; pdb.set_trace()
 import sys
 
-def iris_scatter(filename):
+# define x and y
+x = str(sys.argv[2])
+y = str(sys.argv[3])   
+
+def iris_scatter(filename, x, y):
 # import packages 
     import pandas as pd
     import matplotlib.pyplot as plt
@@ -11,11 +15,10 @@ def iris_scatter(filename):
 
 # import dataset
     filename = pd.read_csv(sys.argv[1])
-   
 # create the scatterplots with regression line    
 # Explanation of the commands: hue = from which variable the groups are defined, x and y define the variables for the x and y axis, ci = this option adds a confidence interval, height changes the size of the output, x/ylabel = label x/y axis,title = adds a titel, show = the output is shown but not saved, savefig = figure is saved  
     sns.set_style("whitegrid")
-    sns.lmplot(data=filename, hue="species", x="petal_length_cm", y="sepal_length_cm", ci=None, height=6)
+    sns.lmplot(data=filename, hue="species", x="x", y="y", ci=None, height=6)
     plt.xlabel("Pengulin")
     plt.ylabel("Dodo")
     plt.xlim(0,8)
@@ -26,5 +29,5 @@ def iris_scatter(filename):
     plt.savefig("scatterplots.png")
 
 if __name__ == '__main__':
-    iris_scatter(sys.argv[1])
+    iris_scatter(filename, x, y)
 
